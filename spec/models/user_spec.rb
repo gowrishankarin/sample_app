510f64e00b621c22f53d6d24f7eb78b2eba979aa
Gowri Gary Shankar <gowrishankar_m@hotmail.com>
2014-07-29 22:16:51 +0530

    User model test spec updated

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	
+	before { @user = User.new(name: "Example User", email: "user@example.com")}
+
+	subject { @user }
+
+
+	it "should respond_to 'name'" do
+		expect(@user).to respond_to(:name)
+	end
+
+	it "should respond_to 'email'" do
+		expect(@user).to respond_to(:email)
+	end
+
 end
