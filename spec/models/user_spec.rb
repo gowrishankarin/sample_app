46c6a3d3084e842f3382f0111ddcd25609a4c132
Gowri Gary Shankar <gowrishankar_m@hotmail.com>
2014-07-30 23:17:09 +0530

    Length and Presence validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,17 +2,22 @@ require 'spec_helper'
 
 describe User do
 	
-	before { @user = User.new(name: "Example User", email: "user@example.com")}
+	before do
+		@user = User.new(name: "Example User", email: "user@example.com")
+	end
 
 	subject { @user }
 
 
-	it "should respond_to 'name'" do
-		expect(@user).to respond_to(:name)
-	end
+	it { should respond_to(:name) }
 
-	it "should respond_to 'email'" do
-		expect(@user).to respond_to(:email)
+	it { should respond_to(:email) }
+
+	it { should be_valid }
+
+	describe "when name is not present" do
+		before { @user.name = "a" * 51}
+		it { should_not be_valid }
 	end
 
 end
