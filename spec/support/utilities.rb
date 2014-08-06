d11c2cd8c214c4fec9f9617f8a400ffa73e4c060
Gowri Gary Shankar <gowrishankar_m@hotmail.com>
2014-08-06 23:07:21 +0530

    User profile edit page added

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -2,7 +2,7 @@
 # @Author: Gowri Gary Shankar
 # @Date:   2014-07-28 22:08:21
 # @Last Modified by:   Gowri Gary Shankar
-# @Last Modified time: 2014-07-28 22:09:46
+# @Last Modified time: 2014-08-06 22:30:07
 
 
 def full_title(page_title) 
@@ -11,5 +11,19 @@ def full_title(page_title)
 		base_title
 	else
 		"#{base_title} | #{page_title}"
+	end
+end
+
+def sign_in(user, options={})
+	if options[:no_capybara]
+		# Sign in when not using no_capybara
+		remember_token = User.new_remember_token
+		cookies[:remember_token] = remember_token;
+		user.update_attribute(:remember_token, User.digest(remember_token))
+	else
+		visit signin_path
+		fill_in "Email", with: user.email 
+		fill_in "Password", with: user.password 
+		click_button "Sign in"
 	end
 end
