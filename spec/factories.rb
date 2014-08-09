5ae6b2183059d086efd591875fedb6a4056d7750
Gowri Gary Shankar <gowrishankar_m@hotmail.com>
2014-08-09 23:32:08 +0530

    Pagination, all user page etc

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,12 +2,12 @@
 # @Author: Gowri Gary Shankar
 # @Date:   2014-08-02 19:47:17
 # @Last Modified by:   Gowri Gary Shankar
-# @Last Modified time: 2014-08-02 19:53:27
+# @Last Modified time: 2014-08-09 23:06:40
 
 FactoryGirl.define do 
 	factory :user do 
-		name	"Michael Hartl" 
-		email 	"michael@example.com"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com"}
 		password "foobar"
 		password_confirmation "foobar"
 	end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -73,7 +73,24 @@ describe "Authentication" do
 					before { patch user_path(user) }
 					specify { expect(response).to redirect_to(signin_path) }
 				end
+
+				describe "visiting the user index" do
+					before { visit users_path }
+					it { should have_title('Sign in') }
+				end
 			end
+		end
+
+		describe "with valid information" do
+			let(:user) { FactoryGirl.create(:user) }
+			before { sign_in user }
+
+			it { should have_title(user.name) }
+			it { should have_link('Users', 	href: users_path)}
+			it { should have_link('Profile', href: user_path(user)) }
+			it { should have_link('Settings', href: edit_user_path(user)) }
+			it { should have_link('Sign out', href: signout_path) }
+			it { should have_link('Sign in', href: signin_path) }
 		end
 
 		describe "as wrong user" do 
