b4913153141aa640f2069c7d1e45a2eb4b012ff9
Gowri Gary Shankar <gowrishankar_m@hotmail.com>
2014-08-12 19:52:24 +0530

    Micropost refinements and content validation

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,12 +2,12 @@
 # @Author: Gowri Gary Shankar
 # @Date:   2014-08-02 19:47:17
 # @Last Modified by:   Gowri Gary Shankar
-# @Last Modified time: 2014-08-10 19:01:32
+# @Last Modified time: 2014-08-12 19:30:15
 
 FactoryGirl.define do 
 	factory :user do 
 		sequence(:name) { |n| "Person #{n}" }
-		sequence(:email) { |n| "person_#{n}@example.com"}
+		sequence(:email) { |n| "person_#{n}@example.com" }
 		password "foobar"
 		password_confirmation "foobar"
 	
@@ -15,5 +15,10 @@ FactoryGirl.define do
 		factory :admin do
 			admin true
 		end
+	end
+
+	factory :micropost do
+		content "Lorem ipsum"
+		user 
 	end
 end
